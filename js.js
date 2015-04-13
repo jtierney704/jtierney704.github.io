1d99cace704d4a1d12b08ad61120faad6359780b
jtierney704 <jessicatierney0407@gmail.com>
2015-04-13 12:36:23 +0200

    asd

--- js.js
+++
@@ -1,8 +0,0 @@
-var noise = document.querySelector("#noise");
-var circle = document.querySelector("#circle");
-
-circle.addEventListener("click", playnoise);
-
-function playnoise(){
-    noise.play();
-}
