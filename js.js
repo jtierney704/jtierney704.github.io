4daf2eaf4e8c26865fc58ed8c49cf6d1bc985e88
jtierney704 <jessicatierney0407@gmail.com>
2015-04-13 12:33:09 +0200

    asd

---
+++ js.js
@@ -0,0 +1,8 @@
+var noise = document.querySelector("#noise");
+var circle = document.querySelector("#circle");
+
+circle.addEventListener("click", playnoise);
+
+function playnoise(){
+    noise.play();
+}
